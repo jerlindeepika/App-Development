57a72977dd73e51dfae1d9793b00360e5407b293
gijendrap <144902090+gijendrap@users.noreply.github.com>
2023-09-14 22:37:28 +0530

    Add files via upload

---
+++ Day_3/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+/**
+ *
+ * Variables
+ */
+const signupButton = document.getElementById("signup-button"),
+  loginButton = document.getElementById("login-button"),
+  userForms = document.getElementById("user_options-forms");
+
+/**
+ * Add event listener to the "Sign Up" button
+ */
+signupButton.addEventListener(
+  "click",
+  () => {
+    userForms.classList.remove("bounceRight");
+    userForms.classList.add("bounceLeft");
+  },
+  false
+);
+
+/**
+ * Add event listener to the "Login" button
+ */
+loginButton.addEventListener(
+  "click",
+  () => {
+    userForms.classList.remove("bounceLeft");
+    userForms.classList.add("bounceRight");
+  },
+  false
+);
